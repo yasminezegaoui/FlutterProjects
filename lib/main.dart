d1d19e5e5ef6fd1ced371be7fe2b6e380649f1d1
yasminezegaoui <yasminezegaoui1@gmail.com>
2024-07-26 09:18:31 +0100

    update2

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,18 @@ class NinjaCard extends StatelessWidget {
       body: const Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('../assets/Chun-li.webp'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Color.fromARGB(249, 117, 117, 117),
+            ),
             Text(
               'NAME',
               style: TextStyle(
@@ -34,12 +45,50 @@ class NinjaCard extends StatelessWidget {
                 letterSpacing: 2.0,
               ),
             ),
+            SizedBox(height: 10.0),
              Text(
-              'Yasmine-Zegaoui',
+              'Chun-Li',
               style: TextStyle(
                 color: Colors.amberAccent,
                 letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
               ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+             Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Color.fromARGB(244, 189, 189, 189),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'chun.li@thenetninja.co.uk',
+                  style: TextStyle(
+                    color: Color.fromARGB(244, 189, 189, 189),
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
